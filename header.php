fa4a7f87a815260bdb7d371d254e53ab11de9f7f
rominavegaolivares <romina.vega@ug.uchile.cl>
2017-06-12 13:59:29 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$tittle="Maternidad Obligada";
+$description="Construcción de un ideal de maternidad en la infancia por medio de juegos y juguetes infantiles";
+$img="img/home-bg.jpg";
+ ?>
+<head>
+
+  <!-- COMMON TAGS -->
+<meta charset="utf-8">
+<title>Maternidad Obligada</title>
+<!-- Search Engine -->
+<meta name="description" content="<?php echo($description);?>">
+<meta name="image" content="<?php echo($img);?>">
+<!-- Schema.org for Google -->
+<meta itemprop="name" content="<?php echo($tittle);?>">
+<meta itemprop="description" content="<?php echo($description);?>">
+<meta itemprop="image" content="<?php echo($img);?>">
+<!-- Twitter -->
+<meta name="twitter:card" content="summary">
+<meta name="twitter:title" content="<?php echo($tittle);?>">
+<meta name="twitter:description" content="<?php echo($description);?>">
+<!-- Open Graph general (Facebook, Pinterest & Google+) -->
+<meta name="og:title" content=<?php echo($tittle);?>">
+<meta name="og:description" content="<?php echo($description);?>">
+<meta name="og:image" content="<?php echo($img);?>">
+<meta name="og:url" content="maternidadoblidada.cl">
+<meta name="og:site_name" content="<?php echo($tittle);?>">
+<meta name="og:locale" content="es_CL">
+<meta name="og:type" content="website">
+
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/modern-business.css" rel="stylesheet">
+        <link href="style.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Archivo+Black" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Taviraj:400,600" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">Maternidad obligada</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+
+                    <li>
+                        <a href="bibliografia.php">Bibliografía</a>
+                    </li>
+                    <li>
+                        <a href="visualizaciones.php">Visualizaciones</a>
+                    </li>
+                    <li>
+                        <a href="antecedentes.php">Antecedentes</a>
+                    </li>
+
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
---
+++ visualizaciones.php
@@ -0,0 +1,27 @@
+<?php include("header.php");?>
+
+<article class="row">
+<div class="col-md-8 col-md-offset-2">
+  <h3>Clasificación de "Femenino" según juguete</h3>
+  <p><img src="img/femenino.svg" class="img-responsive"></p>
+    <p>Encuesta realizada a niños y niñas de entre 3 a 7 años, se les preguntó por la percepción del género para diferentes tipos de juguetes que se venden comúnmente en el mercado.</p>
+    <p><a href="http://aipo.es/articulos/5/1385.pdf">Fuente</a> </p>
+    <hr>
+    <h3>Clasificación de "Masculino" según juguete</h3>
+    <p><img src="img/masculino.svg" class="img-responsive"></p>
+      <p>Encuesta realizada a niños y niñas de entre 3 a 7 años, se les preguntó por la percepción del género para diferentes tipos de juguetes que se venden comúnmente en el mercado.</p>
+      <p><a href="http://aipo.es/articulos/5/1385.pdf">Fuente</a> </p>
+    <hr>
+      <h3>Clasificación de "Neutro" (para ambos géneros) según juguete</h3>
+      <p><img src="img/neutro.svg" class="img-responsive"></p>
+        <p>Encuesta realizada a niños y niñas de entre 3 a 7 años, se les preguntó por la percepción del género para diferentes tipos de juguetes que se venden comúnmente en el mercado. Se puede ver que sorpresivamente los juguetes que los niños y niñas más clasifican como "neutros", son los juegos de construcción y de medicina.</p>
+        <p><a href="http://aipo.es/articulos/5/1385.pdf">Fuente</a> </p>
+    <hr>
+          <h3>Nacimientos según edad de la madre </h3>
+          <p><img src="img/madres-edad.svg" class="img-responsive"></p>
+            <p>Estadística de los nacimientos del 2015 en relación a la edad de las madres. Se puede notar que el rango de edad en dónde más nacimientos se da, es entre los 20 y 29 años. </p>
+            <p><a href="http://www.ine.cl/estadisticas/menu-sociales/genero">Fuente</a> </p>
+</div>
+</article>
+
+  <?php include("footer.php");?>
